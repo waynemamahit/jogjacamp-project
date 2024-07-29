d51ed74077573b6184d05dfc9afa8de21311ac49
waynemamahit <wayne.mamahit@gmail.com>
2024-07-29 20:42:44 +0700

    Adding main layout

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import TestLayoutFormPage from "./pages/TestLayoutFormPage";
 import TestLayoutPage from "./pages/TestLayoutPage";
 import TestSelectPage from "./pages/TestSelectPage";
+import MainLayout from "./components/layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
@@ -9,17 +10,22 @@ const router = createBrowserRouter([
     element: <Navigate to="/test-layout" replace />,
   },
   {
-    path: "/test-layout",
-    Component: TestLayoutPage,
-  },
-  {
-    path: "/test-layout-form",
-    Component: TestLayoutFormPage,
-  },
-  {
-    path: "/test-select",
-    Component: TestSelectPage,
-  },
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/test-layout",
+        Component: TestLayoutPage,
+      },
+      {
+        path: "/test-layout-form",
+        Component: TestLayoutFormPage,
+      },
+      {
+        path: "/test-select",
+        Component: TestSelectPage,
+      },
+    ]
+  }
 ]);
 
 export default router;
